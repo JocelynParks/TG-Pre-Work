53595a4bf74afce15e454f6b01fbbc57dc86bd96
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-09 18:59:40 -0500

    Producing an output to a function using "return"

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Return.js
@@ -0,0 +1,10 @@
+
+function monitorCount (rows, columns) {
+
+return rows*columns;
+}
+const numOfMonitors = monitorCount(5, 4);
+
+ console.log(numOfMonitors);
+
+//Automatically, a called function is determined "undefined". With the use of the keyword "return", functions will produce an output
