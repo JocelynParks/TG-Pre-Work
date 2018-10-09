5e877fb1d916eb3dca30576031fa20c3ce8c4109
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-08 22:02:56 -0500

    Using var, let, and const keywords to create variables

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/Variables.js
@@ -0,0 +1,2 @@
+
+//A variable is a container for a value which hold information. There is also the option for a vriable to label data. In this lesson, I wil learn to use var, let, and const keywords to create variables.
