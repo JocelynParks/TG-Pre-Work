d9fbad55fd5d716b39cbd8de01a25bb2992866b9
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-09 07:47:52 -0500

    Learning more about "if else" or conditional statements

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/What-are-Conditional-Statements.js
@@ -0,0 +1,2 @@
+//The idea of "if-else" decisions are considered conditional statements. Similar to us, these same type of decisions may be replicated with code. "A conditional statement checks specific condition(s) and performs a task based on the condition(s)."
+ 
