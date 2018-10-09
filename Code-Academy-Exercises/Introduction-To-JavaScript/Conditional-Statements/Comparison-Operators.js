fdb95545c4083d9557a72fba2299fd221cfb14df
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-09 08:33:33 -0500

    Writing conditional statements using comparison operators

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/Comparison-Operators.js
@@ -0,0 +1,7 @@
+let hungerLevel = 7;
+if (hungerLevel > 7) {
+ console.log('Time to eat!')
+  } else {
+ console.log('We can eat later!')
+               }
+//Comparison values are used to compare values. The following may be used: <, >, <=, >=, === (equal to), !== (not equal to). Values must be able to be compared and separated with an operator.
