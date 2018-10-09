c63258dc07f1ca7f2083b9b70255d5495fbd6726
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-09 17:54:26 -0500

    To call a function (ex. sayThanks();) copy and paste as any times as needed

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Calling-a-Function.js
@@ -0,0 +1,10 @@
+function sayThanks () {
+  console.log('Thank you for your purchase! We appreciate your business.');
+}
+sayThanks();
+
+sayThanks();
+sayThanks();
+sayThanks();
+
+//A function declaration binds a function to an identifier,therefore the code is not run. In order to run the code, it must be "called". To call multiple times, copy and paste as many times as needed.
