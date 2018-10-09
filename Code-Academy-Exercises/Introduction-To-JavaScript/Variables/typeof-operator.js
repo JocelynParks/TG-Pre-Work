514cbd9e5a1445de9718c3f59908cab92eb66490
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-09 07:43:31 -0500

    Using "typeof" operator to create new types

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/typeof-operator.js
@@ -0,0 +1,6 @@
+let newVariable = 'Playing around with typeof.';
+console.log(typeof newVariable);
+newVariable = 1
+console.log(typeof newVariable)
+
+//Typeof operator will check the variable data types.I used typeof for a newVariable and then set it equal to 1. I then assigned it a new value thus creating a  new type.
