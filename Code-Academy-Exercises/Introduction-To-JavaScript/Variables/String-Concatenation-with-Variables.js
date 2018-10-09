b8aa8b89db846f1c321657b9f33398dbc89ce27d
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-09 07:15:02 -0500

    Using "+" to string concatenation with variable

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/String-Concatenation-with-Variables.js
@@ -0,0 +1,4 @@
+let favoriteAnimal='Dog'
+console.log('My favorite animal' + favoriteAnimal);
+
+//Moving from stringing variables to how to connect or concatenate strings in variables (by using +). Example was My favorite animal (favoriteAnimal): Dog
