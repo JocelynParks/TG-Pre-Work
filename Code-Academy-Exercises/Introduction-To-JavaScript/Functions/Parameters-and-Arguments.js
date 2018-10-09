e37577f767e6b8aeb05d6c6efbfcd642ebc3718a
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-09 18:22:04 -0500

    Using a thank you function and personalizing messages by inserting customers' names in "thank you" messages.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Parameters-and-Arguments.js
@@ -0,0 +1,8 @@
+function sayThanks(name) {
+  console.log('Thank you for your purchase '+ name + '! We appreciate your business.');
+}
+
+sayThanks('Cole');
+
+
+// In this example, a business wants to send thank yous to customers but in a personalized way (using names).I created a function and then Within the "thank-you text", I wrote +name+ so each customer name would be inputted.
