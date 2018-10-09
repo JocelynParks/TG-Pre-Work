02e04c1875f13a33f7678272c7203a4549751188
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-09 18:38:41 -0500

    Creating a function for default values (values that don't change)

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Default-Parameters.js
@@ -0,0 +1,8 @@
+function makeShoppingList( item1 = 'milk', item2 = 'bread', item3 = 'eggs') {
+
+  console.log(`Remember to buy ${item1}`);
+  console.log(`Remember to buy ${item2}`);
+  console.log(`Remember to buy ${item3}`);
+}
+
+//Default parameters use the = operator to assign a parameter a name. In this example, bread, milk, and eggs are "default values"
