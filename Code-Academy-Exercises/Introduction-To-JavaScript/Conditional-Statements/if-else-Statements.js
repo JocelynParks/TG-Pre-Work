5dc4f65cd48f5205bbfd0202232631b753e19b6c
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-09 08:18:20 -0500

    Setting a condition to false and running it with "if else"

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/if-else-Statements.js
@@ -0,0 +1,11 @@
+let sale = true;
+
+sale = false;
+
+if(sale) {
+  console.log('Time to buy!');
+} else {
+  console.log('Time to wait for a sale.')
+}
+
+//With if else statements, you are able to deem conditions false and the code will still run. "Else" adds a default behavior to an "if" statement. They must be paired together followed by {the statement}. This function is useful for binary decisions (yes-or-no questions).
