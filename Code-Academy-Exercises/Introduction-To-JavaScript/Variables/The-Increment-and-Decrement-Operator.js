b58b473e761bccd2a6a9c4a4f26fbc66eaa91578
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-09 07:07:28 -0500

    Increasing and decreasing values by 1

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/The-Increment-and-Decrement-Operator.js
@@ -0,0 +1,12 @@
+let gainedDollar = 3;
+gainedDollar++;
+console.log(gainedDollar);
+
+let lostDollar = 50;
+lostDollar--;
+console.log(lostDollar);
+
+
+
+//Increment (++) and Decrement (--) operators increase or decrease values by 1. Log to the console afterwards.
+ 
