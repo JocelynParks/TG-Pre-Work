fa15359945d51fba7cda8b9213c97290c1ed4b02
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-09 17:20:47 -0500

    Learning to create and use functions to make life as a programmer easier.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/What-are-functions.js
@@ -0,0 +1,2 @@
+
+//Functions are useful due to the fact that programmers may need to create a block of code and then reuse it many times.In the following lesson, I will learn how to create and use these functions.
