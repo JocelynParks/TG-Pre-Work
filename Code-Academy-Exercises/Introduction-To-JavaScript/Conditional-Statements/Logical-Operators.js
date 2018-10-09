3a7e09af0fe133ebda620d88320249b7440771a3
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-09 08:45:58 -0500

    Boolean values (true or false) and logical operators

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/Logical-Operators.js
@@ -0,0 +1,10 @@
+let mood = 'sleepy';
+
+let tirednessLevel = 6;
+if (mood === 'sleepy' && tirednessLevel > 8){
+console.log ('time to sleep')
+} else {
+console.log ('not bed time yet')
+}
+
+// Creating more sophisticated logic by using Boolean values (true or false) and logical operators. There are three logical operators: and (&&)- must be a true statement, or (||)- only one condition must be true, not (!)- reverses the value from true to false or vice versa.
