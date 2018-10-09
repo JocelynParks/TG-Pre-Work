2a2dfa04537abf92e4b5fc08f60d95690a9625f2
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-08 21:42:09 -0500

    Using methods: console.log('Codecademy'.toUpperCase());

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Methods.js
@@ -0,0 +1,6 @@
+// Use .toUpperCase() to log 'Codecademy' in all uppercase letters
+console.log('Codecademy'.toUpperCase());
+
+// Use a string method to log the following string without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim());
+//Methods are actions that may be performed. Call or use these methods by using the dot operator.
