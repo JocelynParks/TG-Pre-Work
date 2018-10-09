04c087cd7ea08a3aa19b0754d4957e21b9014f97
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-09 07:58:06 -0500

    Using the if keyword to create statements and setting to true and false.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/The-if-keyword.js
@@ -0,0 +1,6 @@
+let sale = false;
+
+if (sale){
+  console.log('Time to buy!')
+}
+//The "if" keyword is based on the logic: "if x, then y." In programming, there must be a {code block and a (condition).If the condition is true, the code runs or executes.
