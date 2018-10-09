97cf8069372e30b7d1b410ec101e515c94a92292
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-09 15:50:52 -0500

    Using a Ternary operator (?) instead of "if...then"

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/Ternary-Operator.js
@@ -0,0 +1,22 @@
+let isLocked = false;
+
+isLocked ?
+  console.log('You will need a key to open the door.'):
+
+  console.log('You will not need a key to open the door.');
+
+
+let isCorrect = true;
+
+isCorrect ?
+  console.log('Correct!')
+:
+  console.log('Incorrect!')
+
+let favoritePhrase = 'Love That!';
+
+favoritePhrase === 'Love That!' ?
+  console.log('I love that!')
+:
+console.log("I don't love that!");
+//Ternary Operator is used to simplify if else statements. To use this funtion, follow the condition with a "?" and log the conditions and seperate each with a ":". Statements may be deemed true or false.
