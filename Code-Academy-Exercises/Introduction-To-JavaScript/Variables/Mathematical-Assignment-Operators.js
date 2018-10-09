12eebf1c372a6e74c42119e25f641a2b4b8a06ed
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-09 07:03:10 -0500

    Assigning mathematical values to variables to calculate new values

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/Mathematical-Assignment-Operators.js
@@ -0,0 +1,25 @@
+let levelUp = 10;
+levelUp += 5;
+console.log(levelUp);
+let powerLevel = 9001;
+powerLevel -= 100
+console.log(powerLevel);
+let multiplyMe = 32;
+multiplyMe *= 11;
+console.log(multiplyMe);
+let quarterMe = 1152;
+quarterMe /= 4;
+console.log(quarterMe);
+
+
+
+
+
+
+// These console.log() statements below will help you check the values of the variables.
+// You do not need to edit these statements.
+console.log('The value of levelUp:', levelUp);
+console.log('The value of powerLevel:', powerLevel);
+console.log('The value of multiplyMe:', multiplyMe);
+console.log('The value of quarterMe:', quarterMe);
+//Assigning mathematical values to variables to calculate new values. To reassign use: +==, -=, *=, and /=
