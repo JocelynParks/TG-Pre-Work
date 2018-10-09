a0b9447e9118026dfb36dbf916e0d02ed853abf8
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-09 07:36:20 -0500

    Using string interpolation to create two-part sentences.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/String-Interpolation.js
@@ -0,0 +1,5 @@
+let myName = "Jocelyn";
+let myCity = "Chicago";
+console.log (`My name is ${myName}. My favorite city is ${myCity}.`)
+
+//A template literal is used to log strings together. It is wrapped with `` and inside use a placeholder that looks like this: ${x}.
