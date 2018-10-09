61a6098d040473bff41717b0f02d4b078072fb71
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-08 20:55:43 -0500

    Logging information using console.log (x)

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Console.js
@@ -0,0 +1,4 @@
+console.log(24);
+console.log(1);
+
+/*A console displays information that a programmer needs to know. The console keyword refers to an object, a collection of data and actions, that we can use in our code. The .log() method is built into the console (console.log()).*/
