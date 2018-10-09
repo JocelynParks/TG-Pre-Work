4cc1d46216670c4e04e7f17dfb56dbd518458d9f
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-09 17:43:50 -0500

    Creating a function using function declaration

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Function-Declaration.js
@@ -0,0 +1,8 @@
+function getReminder () {
+console.log('Water the plants.');
+}
+function greetInSpanish() {
+console.log('Buenas Tardes');
+}
+
+//Creating a function using function declaration. Create function, add (), then, brackets where you will log the value.
