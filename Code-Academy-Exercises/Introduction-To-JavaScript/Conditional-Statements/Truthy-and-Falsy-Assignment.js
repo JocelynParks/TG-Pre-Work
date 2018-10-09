528bec2943db444ea7333e7c682b23c1740f2fb4
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-09 08:51:27 -0500

    Checking Non boolean data types (like strings or numbers) without setting a specific value

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/Truthy-and-Falsy-Assignment.js
@@ -0,0 +1,17 @@
+let wordCount = 0;
+
+if (wordCount = 3) {
+  console.log("Great! You've started your work!");
+} else {
+  console.log('Better get to work!');
+}
+
+
+let favoritePhrase = '';
+
+if (favoritePhrase) {
+  console.log("This string doesn't seem to be empty.");
+} else {
+  console.log('This string is definitely empty.');
+}
+//Checking Non boolean data types (like strings or numbers) without setting a specific value. Falsy values (values that are false when checked as a condition) are: 0, "" or'', null, undefined, NaN (not a number)
