30d29cb88a8cc9fc4db65e27b6361d826d9359b9
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-10 17:42:06 -0500

    Storing arrays with other arrays [[nested arrays]]

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/Nested-Arrays.js
@@ -0,0 +1,3 @@
+const numberClusters = [[1, 2], [3, 4], [5, 6]];
+const target = numberClusters [2][1];
+//Nested arrays is essentially arrays stored within other arrays. We can represent this in code by placing [] within []. The exercise wanted "6", so you count the brackets, 0,1,2 (6 is in 2), and then 5,6 is 0,1 so the numbers you want are [2] and [1]
