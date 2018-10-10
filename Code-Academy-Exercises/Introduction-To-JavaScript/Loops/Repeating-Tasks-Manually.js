4bf066b1d6f9223c63424249d51860dfa70b6c0b
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-10 18:16:43 -0500

    Elminating rewriting code over and over with loops

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Repeating-Tasks-Manually.js
@@ -0,0 +1,7 @@
+// Write your code below
+vacationSpots = ['Spain', 'New Zealand', 'Sydney'];
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);
+
+// I defined three places I wanted to visit. Then of course, I had to log them. Not imagaine if there were 100. This is were loops come in to play.
