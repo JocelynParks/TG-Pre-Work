ec0d128a085505a0982221d967fa138690481600
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-10 18:29:55 -0500

    Looping in reverse using => and --

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Looping-in-Reverse.js
@@ -0,0 +1,5 @@
+// The loop below loops from 0 to 3. Edit it to loop backwards from 3 to 0
+for (let counter = 3; counter >= 0; counter--){
+  console.log(counter)
+};
+//Now we will loop in reverse. Similar to last exercise, but now set the stopping condition (use >=) for when the iterator variable is less than the desired amount. Instead of ++, use --. This assignment asked for 3 to 0.
