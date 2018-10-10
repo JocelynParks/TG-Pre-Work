efb0b371726a5fef09432bcae87fe6287a026edd
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-10 17:05:00 -0500

    Removing the last item of an array using the .pop() method

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/The-pop-method.js
@@ -0,0 +1,6 @@
+const chores = ['wash dishes', 'do laundry', 'take out trash', 'cook dinner', 'mop floor'];
+chores.pop('mop floors');
+console.log(chores);
+
+
+//Similar to the .push method, the .pop method is also meant to make life easier. .Pop removes the last items of an array.
