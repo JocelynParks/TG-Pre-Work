1804dd84f70f4457c511ad95ec1f0bb695a1969c
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-10 16:57:31 -0500

    Using the ".length" property within the console.log function

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/The-length-property.js
@@ -0,0 +1,4 @@
+const objectives = ['Learn a new languages', 'Read 52 books', 'Run a marathon'];
+console.log(objectives.length);
+
+//The ".length" property is used when dealing with array. The length function is inserted in the console.log.
