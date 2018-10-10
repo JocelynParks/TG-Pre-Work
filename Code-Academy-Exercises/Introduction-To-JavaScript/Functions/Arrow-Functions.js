0384e08dac565550ac8dc618812dd56eb49dc58b
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-09 20:50:25 -0500

    Using the arrow function instead of writing out "function"

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Arrow-Functions.js
@@ -0,0 +1,11 @@
+const plantNeedsWater = (day) =>
+{
+  if (day === 'Wednesday') {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+//Arrow Functions were created to act as a shortcut to writing "function". It looks something like this:(parameters) =>" followed by brackets.
+ 
