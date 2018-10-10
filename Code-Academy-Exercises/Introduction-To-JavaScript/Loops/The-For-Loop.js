e2274d1f53fce7f60fbcb39ac4221c7a63565a0b
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-10 18:24:00 -0500

    Creating a program that loops from 5 to 10 using the for loop

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/The-For-Loop.js
@@ -0,0 +1,9 @@
+// Write your code below
+for (let counter = 5;
+     counter < 11; counter++) {
+  console.log(counter);
+
+};
+
+
+//The "for" loop includes an iterator variable that usually appears in all three expressions. The iterator variable is initialized, checked against the stopping condition, and assigned a new value on each loop iteration. A for loop contains three expressions separated by ;. This exercise asked for numbers 5 through 10.
