a8cf6ed6358e936d5f110fec48d93c454efcad52
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-09 22:23:31 -0500

    Practicing code with functions and variables within the block scope

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Scope/Block-Scope.js
@@ -0,0 +1,9 @@
+const logVisibleLightWaves = () => {
+  const lightWaves = 'Moonlight';
+  console.log(lightWaves);
+};
+
+logVisibleLightWaves();
+
+
+//Block scope are known as local varables and variables are defined inside of the curly braces. Recieved an error after the code was ran.
