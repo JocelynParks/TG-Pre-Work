b3a21f9efc53090884a7d23e601a19883fbe94b1
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-09 21:28:30 -0500

    Practicing blocks before moving onto to scope.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Scope/Blocks-and-Scope.js
@@ -0,0 +1,10 @@
+const city = 'New York City';
+
+const logCitySkyline = () =>
+{
+  let skyscraper = 'Empire State Building';
+  return 'The stars over the ' + skyscraper + ' in ' + city;
+};
+
+console.log(logCitySkyline());
+//Blocks are the contents found inside of {curly brackets}. You also group one or more statements using blocks. It's important to understans blocks before moving onto to scope.
