2dc0d798c802f793888925eae6705a48e662a820
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-10 18:09:24 -0500

    Using a loop in programming. Will learn more in later lessons.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Loops.js
@@ -0,0 +1,2 @@
+
+//A loop is a tool used to repeat a set of instructions until a specified condition (called "stopping condiition").Loops allow us to create efficient code that automates processes to make scalable, manageable programs.
