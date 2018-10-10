ae528eef9524eef40dac9c21d59daecc659b6b2e
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-10 16:40:29 -0500

    Reassigning values using "let" (not "const")

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/Arrays-with-let-and-const.js
@@ -0,0 +1,7 @@
+let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+
+const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
+condiments[0] = ('Mayo');
+console.log(condiments);
+
+//Remember to declare variables, you may use let and const (used more even though you may not reassign values with this keyword). The above example uses "let" therefore we can reassign an element.
