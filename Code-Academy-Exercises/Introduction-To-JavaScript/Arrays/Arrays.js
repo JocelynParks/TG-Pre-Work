568f5ada96efc18f37c5dd2ff18cc74b84abd07d
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-10 16:10:03 -0500

    Using arrays to list items to help keep content better organized.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/Arrays.js
@@ -0,0 +1,4 @@
+let newYearsResolutions = ['Keep a journal', 'Take a falconry class', 'Learn to juggle'];
+
+console.log(newYearsResolutions);
+//An array is a list that programmers use to store data (each item has a numbered position). The listed items are separated with a comma.
