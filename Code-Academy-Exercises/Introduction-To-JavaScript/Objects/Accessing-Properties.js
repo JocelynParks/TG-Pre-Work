21f9480c29182f1edeadb2cbde21b976018dec06
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-11 14:40:36 -0500

    Assigning an object's property using the "." operator

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Objects/Accessing-Properties.js
@@ -0,0 +1,17 @@
+let spaceship = {
+  homePlanet: 'Earth',
+  color: 'silver',
+  'Fuel Type': 'Turbo Fuel',
+  numCrew: 5,
+  flightPath: ['Venus', 'Mars', 'Saturn']
+};
+
+// Write your code below
+
+const crewCount =
+spaceship.numCrew;
+
+const planetArray =
+spaceship.flightPath;
+
+//Two ways to access an object's property ("."- we write the object's name, followed by the dot operator ). In this example, spaceship is he object and the numCrew (5) is the value.
