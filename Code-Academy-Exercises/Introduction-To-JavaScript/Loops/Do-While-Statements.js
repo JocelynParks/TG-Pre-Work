33720f6dd81be31893025bf463918a253c81c787
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-10 21:07:45 -0500

    Creating a loop that increments using do...while (baking example)

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Do-While-Statements.js
@@ -0,0 +1,9 @@
+// Write your code below
+let cupsOfSugarNeeded = 2;
+let cupsAdded = 0;
+
+do {
+  cupsAdded++
+} while (cupsAdded < cupsOfSugarNeeded);
+
+//A do...while statement while run once on a specific condition after its initial run. A do...while statement says to do a task once and then keep doing it until a specified condition is no longer met. Remember that "while" is different from "do...while".
