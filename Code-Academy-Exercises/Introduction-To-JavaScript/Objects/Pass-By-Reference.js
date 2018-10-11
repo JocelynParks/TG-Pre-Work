916fe1bfdb691c26ef13cae5b0a2d67dfe775040
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-11 18:19:12 -0500

    Changing the object's intital key value

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Objects/Pass-By-Reference.js
@@ -0,0 +1,21 @@
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  homePlanet : 'Earth'
+};
+
+// Write your code below
+
+let greenEnergy = obj => {
+  obj['Fuel Type'] = 'avocado oil';
+}
+
+let remotelyDisable = obj => {
+  obj.disabled = true;
+}
+
+greenEnergy(spaceship);
+
+remotelyDisable(spaceship);
+
+console.log(spaceship)
+//You can create a function changing the object's intital key value. I created a function that takes in a object and then created a function where the key disabled equalled true.
