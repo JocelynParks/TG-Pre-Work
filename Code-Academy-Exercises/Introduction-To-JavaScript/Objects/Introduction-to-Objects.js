80c2864fe306fdd390e423b32bbe4a1cef6dd735
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-11 14:21:49 -0500

    The 7th fundamental data types: Objects

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Objects/Introduction-to-Objects.js
@@ -0,0 +1,1 @@
+// In previous lessons, I learned about the 6 (out of 7) data types: string, number, boolean, null, undefined, and symbol. The 7th is objects. The following lesson will explain more.
