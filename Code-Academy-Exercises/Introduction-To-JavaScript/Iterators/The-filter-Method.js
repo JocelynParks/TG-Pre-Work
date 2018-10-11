d84258adf683d7b56f253e6f072983dd97b2f6c9
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-10 22:53:48 -0500

    Creating a new array with the filter method

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Iterators/The-filter-Method.js
@@ -0,0 +1,22 @@
+const randomNumbers = [375, 200, 3.14, 7, 13, 852];
+
+// Call .filter() on randomNumbers below
+
+
+const favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
+
+
+// Call .filter() on favoriteWords below
+const smallNumbers =
+randomNumbers.filter(function(randomNumbers){
+  return randomNumbers < 250;
+})
+const longFavoriteWords = {
+ favoriteWords.filter(function(favoriteWords)
+      return word.length > 7;
+       )}
+
+//syntax error?
+
+
+//The .filter() method also returns a new array (if deemed 'true').
