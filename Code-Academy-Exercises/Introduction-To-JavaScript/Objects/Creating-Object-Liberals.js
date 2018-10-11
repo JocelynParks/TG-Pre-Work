409e4b25de12ad2228e3231ce58b1284414b836d
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-11 14:31:25 -0500

    Creating object liberals and setting properties

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Objects/Creating-Object-Liberals.js
@@ -0,0 +1,6 @@
+// Write your fasterShip object literal below
+let fasterShip = {
+  'Fuel Type':'Turbo Fuel',
+  color: 'silver'
+}
+//Use {} to designate an object literal. Key-value pairs work together as a key (variable) points to a location in memory (value). Use colon to set descriptor (in this case, 'turbo fuel' and 'silver'). Seperate those two using a comma.
