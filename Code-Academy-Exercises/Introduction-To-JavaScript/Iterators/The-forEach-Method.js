5fa002caf68ceb087eedcbc72d38c6073bc71194
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-10 21:39:57 -0500

    Using the .foreach() method to execute the same code as many times as you set it to

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Iterators/The-forEach-Method.js
@@ -0,0 +1,6 @@
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+// Iterate over fruits below
+fruits.forEach(fruit => console.log(`I want to eat a ${fruits}.`))
+
+
+//.forEach() loops through the array and executes the callback function for each element. The forEach() will always be undefined. You may also use => to represent forEach. When this was ran it read "I want to eat a a,b,c,d" four times.
