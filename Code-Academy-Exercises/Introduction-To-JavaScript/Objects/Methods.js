f211c8d21e2e2b188400378dd2ec0ac2b8a5acf5
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-11 15:16:46 -0500

    Using methods (console.log) and objects

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Objects/Methods.js
@@ -0,0 +1,14 @@
+let retreatMessage = 'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
+
+// Write your code below
+
+let alienShip = {
+	retreat() {
+  	console.log(retreatMessage)},
+  takeOff()
+  {
+  	console.log('Spim... Borp... 		 Glix... Blastoff!')
+}
+};
+
+//A method defines what a object does while a property is what an object has. The combo of console and log is a great example of a method. We can include methods in our object literals by creating ordinary, comma-separated key-value pairs.
