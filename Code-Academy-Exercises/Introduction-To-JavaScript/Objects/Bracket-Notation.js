412322831fac2efd07c45bbb71b695a3e7f06375
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-11 14:50:55 -0500

    Accessing an object's property using the bracket method

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Objects/Bracket-Notation.js
@@ -0,0 +1,15 @@
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  'Active Mission' : true,
+  homePlanet : 'Earth',
+  numCrew: 5
+ };
+
+let propName =  'Active Mission';
+
+// Write your code below
+let isActive =
+spaceship['Active Mission']
+console.log(spaceship['Active Mission'])
+
+//In addition to using ".", you may also use the bracket notation to access an object's property. spaceship is the object and 'Active Mission' is the property.
