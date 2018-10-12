f8065170af62a7b2f55f01c152177c72c1552806
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-12 06:57:10 -0500

    Creating instances and saving it to a constant variable

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Classes/Instance.js
@@ -0,0 +1,11 @@
+class Surgeon {
+  constructor(name, department) {
+    this.name = name;
+    this.department = department;
+  }
+}
+
+const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
+const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
+
+//An instance is an object that contains the property names and methods of a class. In this exercise, I created an instance of the Surgeon class and set the name and the department. Then, created an instance of the surgeon class.
