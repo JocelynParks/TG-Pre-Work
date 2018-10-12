cca89d666aeb6d30d83c158e0a37004949c29248
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-12 10:20:43 -0500

    Final POST Requests

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Requests-2/fetch-POST-Requests-5.js
@@ -0,0 +1,45 @@
+// Information to reach API
+const apiKey = '2c4cee65de76490399470b589ce979ce';
+const url = 'https://api.rebrandly.com/v1/links';
+
+// Some page elements
+const inputField = document.querySelector('#input');
+const shortenButton = document.querySelector('#shorten');
+const responseField = document.querySelector('#responseField');
+
+// AJAX functions
+const shortenUrl = () => {
+  const urlToShorten = inputField.value;
+  const data = JSON.stringify({destination: urlToShorten})
+
+	fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+      'apikey': apiKey
+    },
+    body: data
+  }).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error('Request failed!');
+  }, networkError => {
+    console.log(networkError.message)
+  }).then(jsonResponse => {
+    renderResponse(jsonResponse);
+  })
+}
+
+// Clear page and call AJAX functions
+const displayShortUrl = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild)
+  }
+  shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
+
+// finishing the POST request, this time adding another .then() to the chain to finally make the information available to your webpage
