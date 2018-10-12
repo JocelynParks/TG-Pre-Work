d24e5f3882160cffe54a6ec8c5d7a291324955c1
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-12 10:48:58 -0500

    Combining import statements with the same data

--- Code-Academy-Exercises/Introduction-To-JavaScript/Intermediate-JavaScript-Modules/Combining-Import-Statements.js
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Intermediate-JavaScript-Modules/Combining-Import-Statements.js
@@ -1,1 +1,30 @@
-//The final step before tranpilling our code is to specify a script that initiates the ES6+ to ES5 transpilation. There are several arguments for the following method calls: babel, src, -d, and lib. In this exercise, I ran the babel src -d lib method to transpile our ES6+ code (once again in the pane to the right)
+import { availableAirplanes, flightRequirements, meetsStaffRequirements} from './airplane';
+
+import meetsSpeedRangeRequirements from './airplane';
+
+function displayFuelCapacity() {
+  availableAirplanes.forEach(function(element) {
+    console.log('Fuel Capacity of ' + element.name + ': ' + element['fuelCapacity']);
+  });
+}
+
+displayFuelCapacity();
+
+function displayStaffStatus() {
+  availableAirplanes.forEach(function(element) {
+   console.log(element.name + ' meets staff requirements: ' + meetsStaffRequirements(element.availableStaff, flightRequirements.requiredStaff) );
+  });
+}
+
+displayStaffStatus();
+
+function displaySpeedRangeStatus() {
+  availableAirplanes.forEach(function(element) {
+   console.log(element.name + ' meets speed range requirements:' + meetsSpeedRangeRequirements(element.maxSpeed, element.minSpeed, flightRequirements.requiredSpeedRange));
+  });
+}
+
+displaySpeedRangeStatus();
+
+// you can import the collection of objects and functions with the same data
+// the program doesn't care if you use a combination of different types of import statements
