f0902f53b02b746b34fc43d739ff895a248db72a
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-12 08:55:52 -0500

    Applying wheniuse to search percentage of users and entering as a value

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/caniusedotcom-2.js
@@ -0,0 +1,7 @@
+// Set the variable below to a number
+let esFivePercentageSupport = 97;
+
+// Set the variable below to a number
+let esSixTemplateLiterals = 78;
+
+// I used canIuse and searched the percentage of browsers that support JavaScript ES5 and "Template Literals". Values are 97 and 78.
