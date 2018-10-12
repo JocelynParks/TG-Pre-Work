ef21e5ef09a47b8b017e59ddd2b7807d8533b7f7
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-12 09:19:31 -0500

    Transpiling code when you run npm run in the terminal

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/npm-init.js
@@ -0,0 +1,4 @@
+
+//Now, I will learn to  setup a JavaScript project that transpiles code when you run npm run build from the root directory of a JavaScript project.
+
+//Run in the terminal once again
