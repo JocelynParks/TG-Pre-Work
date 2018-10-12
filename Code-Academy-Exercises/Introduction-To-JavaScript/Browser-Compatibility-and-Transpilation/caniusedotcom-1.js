6dd5d831c8d08fc02bd8e7b1b9f710591d851e51
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-12 08:50:40 -0500

    Entering a ES6 feature to see the relevance amongst users

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/caniusedotcom-1.js
@@ -0,0 +1,2 @@
+
+//In caniuse, you may enter a ES6 feature like 'let' and see the amount (in %) of users that still recognize it. The video in this exaple teaches you how to get started with the tool.
