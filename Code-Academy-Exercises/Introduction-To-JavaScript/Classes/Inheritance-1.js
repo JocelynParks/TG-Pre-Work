25506466e91f66cdbed536a4585f3fb18b2eddeb
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-12 07:42:38 -0500

    Creating a new class (parent) which shares the same properties as an existing class (refered to as the child)

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Classes/Inheritance-1.js
@@ -0,0 +1,1 @@
+//Creating a new class which shares the same properties as an existing class. This is called 'inheritance'. This is a tool that is used to decrese the amount of code that needs to be written. You may create "parent" and "child" (or known as subclasses) classes. The idea is that the child inherents properities from the parent.
