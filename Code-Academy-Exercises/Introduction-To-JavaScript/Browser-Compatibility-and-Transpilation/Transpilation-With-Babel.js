1684279ba9f11b336c0ab00d53463aa6fbcd3852
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-12 09:07:28 -0500

    Transpiling code using Babel by copy code into the terminal window

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/Transpilation-With-Babel.js
@@ -0,0 +1,10 @@
+var pasta = "Spaghetti"; // ES5 syntax
+
+const meat = "Pancetta"; // ES6 syntax
+
+let sauce = "Eggs and cheese"; // ES6 syntax
+
+// Template literals, like the one below, were introduced in ES6
+const carbonara = `You can make carbonara with ${pasta}, ${meat}, and a sauce made with ${sauce}.`;
+
+//So you don't have to manually change code to fit ES5 or ES6 standards, you may use Babel which will do this for you. Instal : npm install babel-cli and npm install babel-preset-env in the TERMINAL WINDOW.
