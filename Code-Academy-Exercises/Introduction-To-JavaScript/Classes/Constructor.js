7de0384395b0903383f60f2dc4a146922ae84874
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-12 06:47:51 -0500

    Using the "constructor" method along with this. and attaching two parameters

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Classes/Constructor.js
@@ -0,0 +1,8 @@
+class Surgeon {
+  constructor(name, department) {
+
+    this._name = name;
+    this._department = department;
+  }
+}
+//This class is "Surgeon" and we want name and department. The constructor method is called everytime there is a new instance of a class. 
