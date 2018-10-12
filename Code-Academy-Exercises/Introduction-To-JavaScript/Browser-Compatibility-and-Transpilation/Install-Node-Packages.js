7a0d7ab7d5643e338498cf72def51b4abb980025
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-12 09:27:10 -0500

    Installing and creating a folder and copies package files onto it

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/Install-Node-Packages.js
@@ -0,0 +1,3 @@
+
+//The 'install' command creates a folder and copies package files onto it. It also installs all of the dependencies for the given packages. Once you npm install packages, you can find the Babel packages and all their dependencies in the respective folder.
+//Once again, this is ran in the terminal. Enter ls, Use npm with the -D flag to install the Babel command line package , Use npm with the -D flag to install the Babel preset environment package.
