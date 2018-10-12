6417074653eecc7e997a1f1fe736b55f114ba8f6
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-12 10:43:01 -0500

    Importing named imports

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Intermediate-JavaScript-Modules/Import-Named-Imports.js
@@ -0,0 +1,30 @@
+import { availableAirplanes, flightRequirements, meetsStaffRequirements} from './airplane';
+
+import meetsSpeedRangeRequirements from './airplane';
+
+function displayFuelCapacity() {
+  availableAirplanes.forEach(function(element) {
+    console.log('Fuel Capacity of ' + element.name + ': ' + element['fuelCapacity']);
+  });
+}
+
+displayFuelCapacity();
+
+function displayStaffStatus() {
+  availableAirplanes.forEach(function(element) {
+   console.log(element.name + ' meets staff requirements: ' + meetsStaffRequirements(element.availableStaff, flightRequirements.requiredStaff) );
+  });
+}
+
+displayStaffStatus();
+
+function displaySpeedRangeStatus() {
+  availableAirplanes.forEach(function(element) {
+   console.log(element.name + ' meets speed range requirements:' + meetsSpeedRangeRequirements(element.maxSpeed, element.minSpeed, flightRequirements.requiredSpeedRange));
+  });
+}
+
+displaySpeedRangeStatus();
+
+// you can import the collection of objects and functions with the same data
+// the program doesn't care if you use a combination of different types of import statements
