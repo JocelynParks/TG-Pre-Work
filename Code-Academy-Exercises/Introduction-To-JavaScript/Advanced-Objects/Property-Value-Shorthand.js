4a0e32b0fe27c9760d9fa76748cafda4f3258b4b
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-12 08:35:43 -0500

    Property value shorthand to refactor a function

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Advanced-Objects/Property-Value-Shorthand.js
@@ -0,0 +1,17 @@
+function robotFactory(model, mobile){
+  return {
+    model,
+    mobile,
+    beep() {
+      console.log('Beep Boop');
+    }
+  }
+}
+
+
+// To check that the property value shorthand technique worked:
+const newRobot = robotFactory('P-501', false)
+console.log(newRobot.model)
+console.log(newRobot.mobile)
+
+// Property Value Shorthand is the short hand for the previous exercise (factory settings). So I changed the function to the shorthand function which eliminates the value from the objects key value pair
