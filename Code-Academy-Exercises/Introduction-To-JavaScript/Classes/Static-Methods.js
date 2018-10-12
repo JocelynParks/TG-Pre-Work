a96a3ea25b52296ff9c962af86c40a3be78d4b82
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-12 08:26:20 -0500

    Create a static method called generatePassword (using Math)

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Classes/Static-Methods.js
@@ -0,0 +1,47 @@
+class HospitalEmployee {
+  constructor(name) {
+    this._name = name;
+    this._remainingVacationDays = 20;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
+  // Create a static method called generatePassword
+  static generatePassword() {
+    return
+
+    Math.floor(Math.random() * 10000);
+  }
+}
+
+class Nurse extends HospitalEmployee {
+  constructor(name, certifications) {
+    super(name);
+    this._certifications = certifications;
+  }
+
+  get certifications() {
+    return this._certifications;
+  }
+
+  addCertification(newCertification) {
+    this.certifications.push(newCertification);
+  }
+}
+
+const nurseOlynyk = new Nurse('Olynyk', ['Trauma','Pediatrics']);
+nurseOlynyk.takeVacationDays(5);
+console.log(nurseOlynyk.remainingVacationDays);
+nurseOlynyk.addCertification('Genetics');
+console.log(nurseOlynyk.certifications);
+
+//Classes may sometimes need to have methods that aren't avaliable in individual instances (but can be called from the class itself). Use the 'static' keyword to create a static method in our Animal Class.
