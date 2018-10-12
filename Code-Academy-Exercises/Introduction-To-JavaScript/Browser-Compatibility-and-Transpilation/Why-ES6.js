ef58a1f1b3aec4b878ddaa392c6680c314f302e8
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-12 09:03:10 -0500

    Changing the code from ES6 to ES5

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/Why-ES6.js
@@ -0,0 +1,9 @@
+var pasta = "Spaghetti"; // ES5 syntax
+
+var meat = "Pancetta"; // ES6 syntax
+
+var sauce = "Eggs and cheese"; // ES6 syntax
+
+// Template literals, like the one below, were introduced in ES6
+var carbonara = `You can make carbonara with" + pasta + meat +, and a sauce made with + sauce +.`;
+//The version ES6 is used for three reasons: similarity to other programming languages, readability and economy of code, and addresses sources of ES5 bugs. In this proplem, I change all the variables to var (let and const are ES6 code). Also (+) signs were used to interpolerate variables.
