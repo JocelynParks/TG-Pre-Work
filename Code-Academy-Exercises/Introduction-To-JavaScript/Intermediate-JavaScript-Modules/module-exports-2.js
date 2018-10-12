ec1a1ce0a59be9cb1b6922cce1ac29253c45caa7
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-12 10:36:47 -0500

    Wrapping any collection of data

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Intermediate-JavaScript-Modules/module-exports-2.js
@@ -0,0 +1,9 @@
+let Airplane = {};
+
+module.exports = {
+  myAirplane: "CloudJet",
+  displayAirplane: function() {
+    return this.myAirplane;
+  }
+};
+//wrap any collection of data and functions in an object, and export the object using module.exports
