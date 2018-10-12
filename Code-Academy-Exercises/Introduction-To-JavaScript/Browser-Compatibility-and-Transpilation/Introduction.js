24890ff4911b219957bb403b0d3b0bb3e7c4c62c
JocelynParks <43864223+JocelynParks@users.noreply.github.com>
2018-10-12 08:47:19 -0500

    Learning how to deal with updated programs and the respective syntax

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/Introduction.js
@@ -0,0 +1,10 @@
+var pasta = "Spaghetti"; // ES5 syntax
+
+const meat = "Pancetta"; // ES6 syntax
+
+let sauce = "Eggs and cheese"; // ES6 syntax
+
+// Template literals, like the one below, were introduced in ES6
+const carbonara = `You can make carbonara with ${pasta}, ${meat}, and a sauce made with ${sauce}.`;
+
+//This lesson reviews how to deal with updated programs and the respective syntax. Two important tools are used: caniuse.com and Babel. The code above includes three features of ES6 syntax: const, let, and ES6 string interpolation.
